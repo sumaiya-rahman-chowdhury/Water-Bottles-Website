d49d13707b893967463630b4c63f1f82a6e0852e
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-09-10 23:51:07 -0700

    created shopping cart + event handler

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import Bottle from './Bottle';
 
 function App() {
   const [bottles, setBottles] = useState([])
+  const [cart,setToCart] = useState([]);
+
+  const addToCart = (bottle)=>{
+    const newCart = [...cart,bottle]
+    setToCart(newCart)
+    console.log(bottle)
+  }
   useEffect(()=>{
     fetch('bottles.json')
     .then(res=>res.json())
@@ -13,9 +20,16 @@ function App() {
     <>
      <div>
      <h1>Water Bottles</h1>
+     <h2>Total :{bottles.length}</h2>
+     <h1>{cart.length}</h1>
+     
+     <ol>{
+      cart.map(item=> <li key={item.id}>{item.name}</li>) 
+     }</ol>
+     
       <div className='container'>
       {
-        bottles.map((bottle)=><Bottle key={bottle.id} bottle={bottle}></Bottle>)
+        bottles.map((bottle)=><Bottle key={bottle.id} bottle={bottle} addToCart={addToCart}></Bottle>)
       }
       </div>
      </div>
--- src/Bottle.jsx
+++ src/Bottle.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+
 import  './Bottle.css'
-const Bottle = ({bottle}) => {
-    console.log(bottle)
+const Bottle = ({bottle,addToCart}) => {
+    // console.log(bottle)
     return (
         <div className='card'>
            <h5>{bottle.name}</h5>
             <img src={bottle.img} alt="" />
            
             <h6>Seller : {bottle.seller}</h6>
-            <p>Price : {bottle.price}</p>
-           
+            <p>Price : {bottle.price}$</p>
+           <button onClick={()=>addToCart(bottle)}>Add To Cart</button>
 
         </div>
     );
